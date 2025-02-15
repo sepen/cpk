cc70cd95e53fefb0f61febed4748f549e5f55f3a
Jose Beneyto <sepen@crux-arm.nu>
2025-02-15 23:20:15 +0100

    Refactoring, improvements and minor fixes

--- src/cpk.cpp
+++ src/cpk.cpp
@@ -74,14 +74,15 @@ void cmd_update(const std::vector<std::string>& args) {
     const std::string index_url = CPK_REPO_URL + "/CPKINDEX";
     const std::string index_file = CPK_HOME_DIR + "/CPKINDEX";
 
-    print_message("Updating index for packages...", BLUE);
-    print_message("Fetching: " + index_url, NONE);
+    print_message("Updating the index of available packages", BLUE);
+    print_message("Fetching " + index_url, NONE);
 
-    if (download_file(index_url, index_file)) {
-        print_message("Update successful", GREEN);
-    } else {
+    if (!download_file(index_url, index_file)) {
         print_message("Failed to update index file: " + index_file, RED);
     }
+
+    int package_count = get_number_of_packages();
+    print_message(std::to_string(package_count)+ " packages available", NONE);
 }
 
 // Function to display package information
@@ -91,94 +92,39 @@ void cmd_info(const std::vector<std::string>& args) {
         return;
     }
 
-    std::string package_name = args[0];
-    std::string index_file = CPK_HOME_DIR + "/CPKINDEX";
-
-    if (!fs::exists(index_file)) {
-        print_message("Package index not found. Run `cpk update` first", RED);
-        return;
-    }
-
-    std::ifstream infile(index_file);
-    std::string index_line;
-    std::string package;
-    std::string pkgname, pkgver, pkgarch, pkgdesc, pkgurl, pkgdeps;
-    bool found = false;
-
-    while (std::getline(infile, index_line)) {
-        if (index_line.find(package_name) != std::string::npos) {
-            found = true;
-
-            size_t cpk_pos = index_line.rfind(".cpk");
-            size_t last_dot = index_line.rfind('.', cpk_pos - 1);
-            size_t hash_pos = index_line.find('#');
-
-            if (cpk_pos != std::string::npos && last_dot != std::string::npos && hash_pos != std::string::npos) {
-                pkgname = index_line.substr(0, hash_pos);
-                pkgver = index_line.substr(hash_pos + 1, last_dot - hash_pos - 1);
-                pkgarch = index_line.substr(last_dot + 1, cpk_pos - last_dot - 1);
-                package = index_line;
-            } else {
-                print_message("Invalid package format", RED);
-            }
-        }
-    }
-
-    if (!found) {
-        print_message("Package not found: " + package_name, RED);
-        return;
-    }
+    std::string package, pkgname, pkgver, pkgarch;
+    if (!find_package(args[0], package, pkgname, pkgver, pkgarch)) return;
 
     std::string package_url = CPK_REPO_URL + "/" + url_encode(package);
-    std::string package_path = CPK_HOME_DIR + "/" + package;
     std::string package_source = CPK_HOME_DIR + "/" + pkgname + "/" + pkgver;
+    std::string package_path = CPK_HOME_DIR + "/" + package;
 
-    // Fetch package and its source
     if (!fs::is_directory(package_source)) {
-        if (download_file(package_url, package_path)) {
-            if (extract_package(package_path, CPK_HOME_DIR)) {
-                print_message("Package source extracted successfuly", NONE);
-            } else {
-                print_message("Failed to extract package info: " + package, RED);
-                return;
-            }
-        }
-        else {
-            print_message("Failed to download package: " + package, RED);
+        if (!download_file(package_url, package_path) || !extract_package(package_path, CPK_HOME_DIR)) {
+            print_message("Failed to retrieve package info", RED);
             return;
         }
     }
 
     std::string pkgfile_path = package_source + "/Pkgfile";
-    std::string package_file;
-
-    // Read Pkgfile
-    if (!fs::exists(pkgfile_path)) {
-        print_message("Pkgfile not found in " + pkgfile_path, RED);
-    } else {
-        if (!parse_pkgfile(pkgfile_path, pkgname, pkgdesc, pkgurl, pkgdeps)) {
-            print_message("Failed to parse Pkgfile", RED);
-        } else {
-
-            package_file = find_pkg_file(package_source, pkgname, pkgver);
-
-            if (fs::exists(package_file)) {
-                // Display parsed values
-                print_message("Name         | " + pkgname, NONE);
-                print_message("Version      | " + pkgver, NONE);
-                print_message("Arch         | " + pkgarch, NONE);
-                print_message("Description  | " + ltrim(pkgdesc), NONE);
-                print_message("URL          | " + ltrim(pkgurl), NONE);
-                print_message("Dependencies | " + ltrim(pkgdeps), NONE);
-            }
-        }
+    std::string pkgdesc, pkgurl, pkgdeps;
+    if (!fs::exists(pkgfile_path) || !parse_pkgfile(pkgfile_path, pkgname, pkgdesc, pkgurl, pkgdeps)) {
+        print_message("Failed to parse Pkgfile", RED);
+        return;
     }
+
+    print_message("Name         | " + pkgname, NONE);
+    print_message("Version      | " + pkgver, NONE);
+    print_message("Arch         | " + pkgarch, NONE);
+    print_message("Description  | " + ltrim(pkgdesc), NONE);
+    print_message("URL          | " + ltrim(pkgurl), NONE);
+    print_message("Dependencies | " + ltrim(pkgdeps), NONE);
 }
 
 // Function to search for a package or description
 void cmd_search(const std::vector<std::string>& args) {
     if (args.empty()) {
-        print_message("Search term is required", RED);
+        print_message("Search argument is required", NONE);
         return;
     }
 
@@ -190,11 +136,11 @@ void cmd_search(const std::vector<std::string>& args) {
         return;
     }
 
-    std::ifstream infile(index_file);
+    std::ifstream file(index_file);
     std::string index_line;
     bool found = false;
 
-    while (std::getline(infile, index_line)) {
+    while (std::getline(file, index_line)) {
         if (index_line.find(search_term) != std::string::npos) {
             found = true;
             print_message(index_line, NONE);
@@ -202,172 +148,93 @@ void cmd_search(const std::vector<std::string>& args) {
     }
 
     if (!found) {
-        print_message("No packages found matching: " + search_term, RED);
+        print_message("No matching packages found", NONE);
     }
+
+    file.close();
 }
 
 // Function to install a package
 void cmd_install(const std::vector<std::string>& args) {
     if (args.empty()) {
-        print_message("Package name is required", RED);
+        print_message("Package name is required", NONE);
         return;
     }
 
-    std::string package_name = args[0];
-    std::string index_file = CPK_HOME_DIR + "/CPKINDEX";
+    std::string package, pkgname, pkgver, pkgarch;
+    if (!find_package(args[0], package, pkgname, pkgver, pkgarch)) return;
 
-    if (!fs::exists(index_file)) {
-        print_message("Package index not found. Run `cpk update` first", RED);
-        return;
-    }
-
-    std::ifstream infile(index_file);
-    std::string index_line;
-    std::string package;
-    std::string pkgname, pkgver, pkgarch, pkgdesc, pkgurl, pkgdeps;
-    bool found = false;
-
-    while (std::getline(infile, index_line)) {
-        if (index_line.find(package_name) != std::string::npos) {
-            found = true;
-
-            size_t cpk_pos = index_line.rfind(".cpk");
-            size_t last_dot = index_line.rfind('.', cpk_pos - 1);
-            size_t hash_pos = index_line.find('#');
-
-            if (cpk_pos != std::string::npos && last_dot != std::string::npos && hash_pos != std::string::npos) {
-                pkgname = index_line.substr(0, hash_pos);
-                pkgver = index_line.substr(hash_pos + 1, last_dot - hash_pos - 1);
-                pkgarch = index_line.substr(last_dot + 1, cpk_pos - last_dot - 1);
-                package = index_line;
-            } else {
-                print_message("Invalid package format", RED);
-            }
-        }
-    }
-
-    if (!found) {
-        print_message("Package not found: " + package_name, RED);
+    if (is_package_installed(pkgname)) {
+        print_message("Package is already installed", NONE);
         return;
     }
 
     std::string package_url = CPK_REPO_URL + "/" + url_encode(package);
-    std::string package_path = CPK_HOME_DIR + "/" + package;
     std::string package_source = CPK_HOME_DIR + "/" + pkgname + "/" + pkgver;
 
-    // Fetch package and its source
     if (!fs::is_directory(package_source)) {
-        if (download_file(package_url, package_path)) {
-            if (extract_package(package_path, CPK_HOME_DIR)) {
-                print_message("Package source extracted successfuly", NONE);
-            } else {
-                print_message("Failed to extract package info: " + package, RED);
-                return;
-            }
-        }
-        else {
-            print_message("Failed to download package: " + package, RED);
+        if (!download_file(package_url, package_source) || !extract_package(package_source, CPK_HOME_DIR)) {
+            print_message("Failed to retrieve package", RED);
             return;
         }
     }
 
-    std::string pkgfile_path = package_source + "/Pkgfile";
-    std::string package_file;
+    std::string package_file = find_pkg_file(package_source, pkgname, pkgver);
+    if (!fs::exists(package_file)) {
+        print_message("Package file not found", RED);
+        return;
+    }
 
-    // Read Pkgfile
-    if (!fs::exists(pkgfile_path)) {
-        print_message("Pkgfile not found in " + pkgfile_path, RED);
-    } else {
-        if (!parse_pkgfile(pkgfile_path, pkgname, pkgdesc, pkgurl, pkgdeps)) {
-            print_message("Failed to parse Pkgfile", RED);
-        } else {
+    if (!run_script(package_source + "/pre-install", "Running pre-install")) {
+        print_message("Pre-install script failed", RED);
+        return;
+    }
 
-            package_file = find_pkg_file(package_source, pkgname, pkgver);
+    print_message("Installing " + package, BLUE);
+    if (shellcmd("pkgadd", {package_file}, nullptr) != 0) {
+        print_message("Failed to install package", RED);
+        return;
+    }
 
-            if (fs::exists(package_file)) {
-                bool install_failed = false;
+    if (!run_script(package_source + "/post-install", "Running post-install")) {
+        print_message("Post-install script failed", RED);
+        return;
+    }
 
-                // pre-install
-                std::string pkg_preinstall_path = package_source + "/pre-install";
-                if (fs::exists(pkg_preinstall_path)) {
-                    print_message("Running pre-install", BLUE);
-                    std::string preinstall_cmd = "sh -x";
-                    std::vector<std::string> preinstall_args = { pkg_preinstall_path };
-                    std::string preinstall_output;
-                    install_failed = shellcmd(preinstall_cmd, preinstall_args, &preinstall_output);
-                    if (install_failed) {
-                        print_message("Failed to run pre-install", RED);
-                    }
-                }
-
-                // pkgadd   
-                print_message("Installing " + package, BLUE);
-                std::string pkgadd_cmd = "pkgadd";
-                std::vector<std::string> pkgadd_args = { package_file };
-                std::string pkgadd_output;
-                install_failed = shellcmd(pkgadd_cmd, pkgadd_args, &pkgadd_output);
-                if (install_failed) {
-                    print_message("Failed to run pkgadd", RED);
-                }
-
-                // post-install
-                std::string pkg_postinstall_path = package_source + "/post-install";
-                if (fs::exists(pkg_postinstall_path)) {
-                    print_message("Running post-install", BLUE);
-                    std::string postinstall_cmd = "sh -x";
-                    std::vector<std::string> postinstall_args = { pkg_postinstall_path };
-                    std::string postinstall_output;
-                    install_failed = shellcmd(postinstall_cmd, postinstall_args, &postinstall_output);
-                    if (install_failed) {
-                        print_message("Failed to run post-install", RED);
-                    }
-                }
-
-                // README
-                std::string pkg_readme_path = package_source + "/README";
-                if (fs::exists(pkg_readme_path)) {
-                    print_message("Printing README", BLUE);
-                    std::string readme_cmd = "cat";
-                    std::vector<std::string> readme_args = { pkg_readme_path };
-                    std::string readme_output;
-                    install_failed = shellcmd(readme_cmd, readme_args, &readme_output);
-                    if (install_failed) {
-                        print_message("Failed to print README", RED);
-                    }
-                }
-
-                if (install_failed) {
-                    print_message("Failed to install package", RED);
-                } else {
-                    print_message("Package installed successfuly", GREEN);
-                }
-            }
+    std::string readme_path = package_source + "/README";
+    if (fs::exists(readme_path)) {
+        print_message("Printing README", BLUE);
+        if (shellcmd("cat", {readme_path}, nullptr) != 0) {
+            print_message("Failed to print README file", RED);
+            return;
         }
     }
+
+    print_message("Package installed successfully", NONE);
 }
 
 // Function to uninstall a package
 void cmd_uninstall(const std::vector<std::string>& args) {
-    bool uninstall_failed = false;
     std::string pkgrm_cmd = "pkgrm";
-    std::vector<std::string> pkgrm_args = {};
+    std::vector<std::string> pkgrm_args = args;
     std::string pkgrm_output;
-    uninstall_failed = shellcmd(pkgrm_cmd, pkgrm_args, &pkgrm_output);
-    if (uninstall_failed) {
-        print_message("Failed to uninstall package", RED);
+    std::string package_name = args[0];
+
+    if (!is_package_installed(package_name)) {
+        print_message("Package " + package_name + " not installed", RED);
+        return;
     }
+
+    shellcmd(pkgrm_cmd, pkgrm_args, &pkgrm_output, false);
 }
 
 // Function to list installed packages
 void cmd_list(const std::vector<std::string>& args) {
-    bool list_failed = false;
     std::string pkginfo_cmd = "pkginfo";
     std::vector<std::string> pkginfo_args = { "-i" };
     std::string pkginfo_output;
-    list_failed = shellcmd(pkginfo_cmd, pkginfo_args, &pkginfo_output);
-    if (list_failed) {
-        print_message("Failed to list installed packages", RED);
-    }
+
+    shellcmd(pkginfo_cmd, pkginfo_args, &pkginfo_output);
 }
 
 // Function to upgrade installed packages
